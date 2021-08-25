6a9ae8acb770889fd02234d60e3f4c855404527e
David Carr <dave@dcblog.dev>
2021-08-25 13:45:49 +0100

    added livewire component to run a PEST test with.

--- Modules/Contacts/Tests/Feature/ContactTest.php
+++ Modules/Contacts/Tests/Feature/ContactTest.php
@@ -4,7 +4,7 @@ use App\Http\Livewire\Counter;
 use function Pest\Livewire\livewire;
 
 it('can be incremented', function () {
-    $this->livewire(Counter::class)
+    livewire(Counter::class)
         ->call('increment')
         ->assertSee(1);
 });
