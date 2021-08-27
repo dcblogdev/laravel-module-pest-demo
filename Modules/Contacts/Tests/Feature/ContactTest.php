7e9da4469bc51055886369b8beffcab0a40eb63c
David Carr <dave@dcblog.dev>
2021-08-27 07:41:38 +0100

    updated counter example and setup a passing pest test

--- Modules/Contacts/Tests/Feature/ContactTest.php
+++ Modules/Contacts/Tests/Feature/ContactTest.php
@@ -3,7 +3,9 @@
 use App\Http\Livewire\Counter;
 use function Pest\Livewire\livewire;
 
-it('can be incremented', function () {
+uses(Tests\TestCase::class);
+
+test('can be incremented', function () {
     livewire(Counter::class)
         ->call('increment')
         ->assertSee(1);
